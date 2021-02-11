4b1e88ab4d680ba8d1a82e3effcbf0b16a530d01
Aloys Berger <aloysberger@gmail.com>
2021-02-11 17:07:40 +0100

    Initial push

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  arrowParens: "avoid",
+  printWidth: 90,
+  svelteBracketNewLine: true,
+  svelteStrictMode: false,
+  trailingComma: "none",
+  plugins: ["prettier-plugin-svelte"],
+};
---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  export let name: string;
+  import Navigation from "./components/Navigation/Navigation.svelte";
+</script>
+
+<div class="container">
+  <Navigation />
+  <main>
+    <section>Illustration</section>
+    <section>Projects</section>
+    <section>Blog</section>
+    <section>Contact</section>
+  </main>
+</div>
+
+<style>
+  .container {
+    display: flex;
+    background: var(--color-darkest);
+    box-sizing: border-box;
+  }
+
+  main {
+    flex: 1 1 auto;
+    overflow: scroll;
+    max-height: 100vh;
+  }
+
+  section {
+    height: 100vh;
+    border: 5px solid red;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
